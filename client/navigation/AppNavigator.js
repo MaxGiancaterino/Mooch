5e24fc6c9d57c52ab5ec82bbd5e585eff4572c42
Maxwell Giancaterino <mgianc@seas.upenn.edu>
2018-07-17 15:06:58 +0000

    added auth navigation

--- client/navigation/AppNavigator.js
+++ client/navigation/AppNavigator.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import React from "react";
+import { createStackNavigator, createSwitchNavigator } from "react-navigation";
 
-import MainTabNavigator from './MainTabNavigator';
+import MainTabNavigator from "./MainTabNavigator";
+import AuthLoadingScreen from "../screens/AuthLoadingScreen";
+import SignInScreen from "../screens/SignInScreen";
 
-export default createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-});
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+export default createSwitchNavigator(
+  {
+    // You could add another route here for authentication.
+    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+    AuthLoading: AuthLoadingScreen,
+    App: MainTabNavigator,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: "AuthLoading"
+  }
+);
